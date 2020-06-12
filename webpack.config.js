2da02097fb82399b17f381204a1e3dce7903a57a
Andrei <9771919@mail.ru>
2020-06-12 13:06:25 +0300

    add plugins for webpack and add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle[hash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'src/dist')
+        }
+      ],
+    }),
+  ]
 }
