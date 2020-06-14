c7ec98fd991fe59cac61566e09e84eada66b8e53
Andrei <9771919@mail.ru>
2020-06-14 10:43:59 +0300

    add content from components to html

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,8 +1,26 @@
 export class Excel {
-
   // $ - point at dom node
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
     this.components = options.components || []
+  }
+
+  getRoot() {
+    const $root = document.createElement('div') // создание главного элемента
+    $root.classList.add('excel') // добавляем класс для корневого элемента
+    // проходимся по всем классам в массиве и создаем их экземпляры
+    this.components.forEach(Component => {
+      const $el = document.createElement('div')
+      $el.classList.add(Component.className)
+      const component = new Component($el)
+      $el.innerHTML = component.toHTML() // наполнение корневого элемента
+      $root.append($el)
+    })
+    return $root
+  }
+
+  // складываем что то в шаблон
+  render() {
+    this.$el.append(this.getRoot())
   }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,9 @@
 export class DomListener {
-
+  // $root - корневой элемент на который будем вешать различные слушатели
+  constructor($root) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener!`)
+    }
+    this.$root = $root
+  }
 }
